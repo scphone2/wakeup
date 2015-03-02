7519858ac7480b20d4802927998f49e2147ccb21
scphone2 <pherzog@silentcircle.com>
2015-03-02 16:46:13 -0500

    Added new logo, cleanup home page

--- public/modules/tokens/config/tokens.client.config.js
+++ public/modules/tokens/config/tokens.client.config.js
@@ -4,7 +4,7 @@
 angular.module('tokens').run(['Menus',
 	function(Menus) {
 		// Set top bar menu items
-		Menus.addMenuItem('topbar', 'Tokens', 'tokens', 'dropdown', '/tokens(/create)?');
+		Menus.addMenuItem('topbar', 'Tokens', 'tokens', 'dropdown', '/tokens(/create)?', true);
 		Menus.addSubMenuItem('topbar', 'tokens', 'List Tokens', 'tokens');
 		Menus.addSubMenuItem('topbar', 'tokens', 'New Token', 'tokens/create');
 	}
